5d57ddc63f44316ed2efc65e3f97942302617084
Raghav Maheshwari <34077214+raghavddps2@users.noreply.github.com>
2019-02-22 23:29:16 +0530

    Update textview.java

--- textview.java
+++ textview.java
@@ -23,3 +23,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+/*
+    Please note: We have three methods, on textChanged, beforeTextChnaged and after text changed.
+    The explanation is given as:
+    
+    
+
+    beforeTextChanged(CharSequence s, int start, int count, int after).
+    This means that the characters are about to be replaced with some new text. The text is uneditable.
+    Use: when you need to take a look at the old text which is about to change.
+
+    onTextChanged(CharSequence s, int start, int before, int count).
+    Changes have been made, some characters have just been replaced. The text is uneditable.
+    Use: when you need to see which characters in the text are new.
+
+    afterTextChanged(Editable s).
+    The same as above, except now the text is editable.
+    Use: when you need to see and possibly edit the new text.
+
+*/
+//https://stackoverflow.com/questions/8543449/how-to-use-the-textwatcher-class-in-android
+//This is a nice resource to learn differnce between three methods.
